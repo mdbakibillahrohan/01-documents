2169690f97155ad2b1460da1c80594006a2afc4f
ktanim <tanim1109135@gmail.com>
2023-07-08 12:44:37 +0600

    review plsql function

--- database/postgres/05-function/04-accounting/R__05_04_00__ledger.sql
+++ database/postgres/05-function/04-accounting/R__05_04_00__ledger.sql
@@ -2,9 +2,9 @@ set client_min_messages = 'warning';
 
 DROP FUNCTION IF EXISTS "public".get_ledger_code(p_ledger_subgroup_oid varchar(128), p_company_oid varchar(128));
 CREATE OR REPLACE FUNCTION "public".get_ledger_code(p_ledger_subgroup_oid varchar(128), p_company_oid varchar(128))
-RETURNS varchar(4) AS $get_ledger_code$
+RETURNS varchar(7) AS $get_ledger_code$
 	DECLARE
-        v_data                                      record;
+        v_data                                  record;
   		v_ledger_code 				            varchar(7);
     BEGIN
         select to_char((to_number(ledger_code, 'FM9999999') + 1), 'FM0000000')
@@ -16,7 +16,7 @@ RETURNS varchar(4) AS $get_ledger_code$
         limit 1;
         if v_ledger_code is null then
             select * into v_data from ledger_subgroup where oid = p_ledger_subgroup_oid and company_oid = p_company_oid;
-            v_ledger_code := concat(v_data.ledger_subgroup_code, '01');
+            v_ledger_code := concat(v_data.ledger_subgroup_code, '001');
         end if;
         return v_ledger_code;
     END;
@@ -35,14 +35,9 @@ RETURNS varchar(128) AS $save_update_ledger$
         v_timestamp                                 varchar(64);
         v_oid                                       varchar(128);
         v_description                               text;
-  		v_ledger_code 				                varchar(4);
     BEGIN
         select get_company_by_login_id(p_json->>'created_by') into v_company;
-        select * into v_data from ledger_subgroup where oid = p_json->>'ledger_subgroup_oid' and company_oid = v_company->>'oid';
-        if v_data.oid is null then
-            return null;
-        end if;
-
+		select * into v_data from ledger_subgroup where oid = p_json->>'ledger_subgroup_oid' and company_oid = v_company->>'oid';
         if length(coalesce(p_json->>'oid', '')) = 0 then
             v_action_type := 'Save';
             select uuid() into v_oid;
@@ -50,25 +45,22 @@ RETURNS varchar(128) AS $save_update_ledger$
             insert into ledger (oid, ledger_code, ledger_name, mnemonic,
                 ledger_type, balance_sheet_item, initial_balance, ledger_balance, ledger_subgroup_oid, status, company_oid, created_by)
             values (v_oid, v_timestamp, p_json->>'ledger_name', p_json->>'mnemonic', v_data.ledger_subgroup_type,
-                v_data.balance_sheet_item, coalesce((p_json->>'initial_balance')::float, 0), 
-                coalesce((p_json->>'initial_balance')::float, 0),
-                p_json->>'ledger_subgroup_oid', p_json->>'status', v_company->>'oid', v_company->>'login_id');
+                v_data.balance_sheet_item, coalesce((p_json->>'initial_balance')::float, 0),
+                coalesce((p_json->>'initial_balance')::float, 0), p_json->>'ledger_subgroup_oid',
+				p_json->>'status', v_company->>'oid', v_company->>'login_id');
         else
             v_action_type := 'Update';
             v_oid := p_json->>'oid';
             select ledger_code into v_timestamp from ledger where oid = v_oid;
             update ledger set ledger_name = p_json->>'ledger_name', mnemonic = p_json->>'mnemonic',
-                initial_balance = coalesce((p_json->>'initial_balance')::float, 0), 
-                ledger_balance = coalesce((p_json->>'initial_balance')::float, 0),
+                initial_balance = coalesce((p_json->>'initial_balance')::float, 0),
                 ledger_subgroup_oid =  p_json->>'ledger_subgroup_oid', ledger_type = v_data.ledger_subgroup_type,
                 balance_sheet_item = v_data.balance_sheet_item, status = p_json->>'status', company_oid = v_company->>'oid',
                 edited_by = v_company->>'login_id', edited_on = clock_timestamp()
             where oid = v_oid;
         end if;
 
-        
         v_description := concat(v_action_type, ' ', p_json->>'ledger_name', ' as ledger by ', v_company->>'login_id');
-
 		insert into activity_log (description, reference_id, reference_name, created_by, company_oid)
 		values (v_description, v_timestamp, 'Ledger', v_company->>'login_id', v_company->>'oid');
         return v_oid;
--- database/postgres/05-function/04-accounting/R__05_04_01__sub_ledger.sql
+++ database/postgres/05-function/04-accounting/R__05_04_01__sub_ledger.sql
@@ -2,7 +2,7 @@ set client_min_messages = 'warning';
 
 DROP FUNCTION IF EXISTS "public".get_subledger_code(p_ledger_oid varchar(128), p_company_oid varchar(128));
 CREATE OR REPLACE FUNCTION "public".get_subledger_code(p_ledger_oid varchar(128), p_company_oid varchar(128))
-RETURNS varchar(4) AS $get_subledger_code$
+RETURNS varchar(10) AS $get_subledger_code$
 	DECLARE
         v_data                                      record;
   		v_subledger_code 				            varchar(10);
@@ -16,7 +16,7 @@ RETURNS varchar(4) AS $get_subledger_code$
         limit 1;
         if v_subledger_code is null then
             select * into v_data from ledger where oid = p_ledger_oid and company_oid = p_company_oid;
-            v_subledger_code := concat(v_data.ledger_code, '01');
+            v_subledger_code := concat(v_data.ledger_code, '001');
         end if;
         return v_subledger_code;
     END;
@@ -37,54 +37,45 @@ RETURNS varchar(128) AS $save_update_subledger$
         v_description                               text;
     BEGIN
         select get_company_by_login_id(p_json->>'created_by') into v_company;
-        select * into v_data from ledger l
-        left join ledger_setting ls on ls.ledger_oid = l.oid 
-        where l.oid = p_json->>'ledger_oid' and l.company_oid = v_company->>'oid';
-        if v_data.oid is null then
-            return null;
-        end if;
+        select * into v_data from ledger where oid = p_json->>'ledger_oid' and company_oid = v_company->>'oid';
 
         if length(coalesce(p_json->>'oid', '')) = 0 then
             v_action_type := 'Save';
             select uuid() into v_oid;
             select get_subledger_code(p_json->>'ledger_oid', v_company->>'oid') into v_timestamp;
-            insert into subledger (oid,  subledger_code, ledger_key, subledger_name, 
-                subledger_type, balance_sheet_item, initial_balance, subledger_balance, ledger_oid, 
-                status, company_oid, created_by)
-            values (v_oid, v_timestamp, v_data.ledger_key, p_json->>'subledger_name', v_data.ledger_type,
-                v_data.balance_sheet_item, coalesce((p_json->>'initial_balance')::float, 0), 
-                coalesce((p_json->>'initial_balance')::float, 0),
-                p_json->>'ledger_oid', p_json->>'status', v_company->>'oid', v_company->>'login_id');
+            insert into subledger (oid, subledger_code, subledger_name, ubledger_type, balance_sheet_item,
+				initial_balance, subledger_balance,  ledger_oid, status, company_oid, created_by)
+            values (v_oid, v_timestamp, p_json->>'subledger_name', v_data.ledger_type,
+                v_data.balance_sheet_item, coalesce((p_json->>'initial_balance')::float, 0),
+                coalesce((p_json->>'initial_balance')::float, 0), p_json->>'ledger_oid',
+				p_json->>'status', v_company->>'oid', v_company->>'login_id');
         else
             v_action_type := 'Update';
             v_oid := p_json->>'oid';
             select subledger_code into v_timestamp from subledger where oid = v_oid;
-            update subledger set subledger_name = p_json->>'subledger_name', ledger_key = v_data.ledger_key,
-                initial_balance = coalesce((p_json->>'initial_balance')::float, 0), 
-                subledger_balance = coalesce((p_json->>'initial_balance')::float, 0),
+            update subledger set subledger_name = p_json->>'subledger_name',
+                initial_balance = coalesce((p_json->>'initial_balance')::float, 0),
                 ledger_oid =  p_json->>'ledger_oid', subledger_type = v_data.ledger_type,
                 balance_sheet_item = v_data.balance_sheet_item, status = p_json->>'status',
                 company_oid = v_company->>'oid', edited_by = v_company->>'login_id', edited_on = clock_timestamp()
             where oid = v_oid;
         end if;
 
-        
         v_description := concat(v_action_type, ' ', p_json->>'subledger_name', ' as subledger by ', v_company->>'login_id');
-
 		insert into activity_log (description, reference_id, reference_name, created_by, company_oid)
-		values (v_description, v_timestamp, 'subledger', v_company->>'login_id', v_company->>'oid');
+		values (v_description, v_timestamp, 'Subledger', v_company->>'login_id', v_company->>'oid');
         return v_oid;
     END;
 $save_update_subledger$ language plpgsql;
 
 
--- select "public".save_update_subledger('{ "oid": "", "subledger_name": "demo subledger", 
--- 									  "initial_balance":"1253245.", 
--- 									  "ledger_oid": "e2cf5bb6-5c96-4f07-b0a3-88c4db541e0d", 
+-- select "public".save_update_subledger('{ "oid": "", "subledger_name": "demo subledger",
+-- 									  "initial_balance":"1253245.",
+-- 									  "ledger_oid": "e2cf5bb6-5c96-4f07-b0a3-88c4db541e0d",
 -- 									  "created_by": "admin" }');
--- select "public".save_update_subledger('{ "oid": "4a266dfb-67e2-4108-b189-43716923a457", "subledger_name": "demo updated subledger", 
--- 									  "initial_balance":"1253245.", 
--- 									  "ledger_oid": "e2cf5bb6-5c96-4f07-b0a3-88c4db541e0d", 
+-- select "public".save_update_subledger('{ "oid": "4a266dfb-67e2-4108-b189-43716923a457", "subledger_name": "demo updated subledger",
+-- 									  "initial_balance":"1253245.",
+-- 									  "ledger_oid": "e2cf5bb6-5c96-4f07-b0a3-88c4db541e0d",
 -- 									  "created_by": "admin" }');
 
 -- PGPASSWORD='celac' psql -h localhost -U celac -d celac -f ./R__05_04_01__sub_subledger.sql
