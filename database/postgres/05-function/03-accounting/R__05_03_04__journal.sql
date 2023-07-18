2cb51bc0edb507196320a9ce24c5e5b0409d6cc6
Nazim Uddin <nazimuddin5390@gmail.com>
2023-07-18 22:33:28 +0600

    done

---
+++ database/postgres/05-function/03-accounting/R__05_03_04__journal.sql
@@ -0,0 +1,105 @@
+set client_min_messages = 'warning';
+
+DROP FUNCTION IF EXISTS "public".get_new_ledger_balance(p_ledger_oid varchar(128), p_debited_amount float, p_credited_amount float);
+CREATE OR REPLACE FUNCTION "public".get_new_ledger_balance(p_ledger_oid varchar(128), p_debited_amount float default 0, p_credited_amount float default 0) 
+RETURNS float AS $get_new_ledger_balance$
+    DECLARE
+        v_ledger_balance                    float;
+        v_debit_type                        varchar(16) := 'Debit';
+        v_credit_type                       varchar(16) := 'Credit';
+    BEGIN 
+        select coalesce ((
+            select (
+                case when ledger_type = v_debit_type and p_debited_amount > p_credited_amount then (ledger_balance + p_debited_amount)
+                when ledger_type = v_debit_type and p_debited_amount < p_credited_amount then (ledger_balance - p_credited_amount)
+                when ledger_type = v_credit_type and p_debited_amount > p_credited_amount then (ledger_balance - p_debited_amount)
+                when ledger_type = v_credit_type and p_debited_amount < p_credited_amount then (ledger_balance + p_credited_amount)
+                else 0 end
+            )
+            from ledger
+            where oid = p_ledger_oid
+        ), 0) into v_ledger_balance;
+        return v_ledger_balance;
+    END;
+$get_new_ledger_balance$ language plpgsql;
+
+DROP FUNCTION IF EXISTS "public".get_new_subledger_balance(p_subledger_oid varchar(128), p_debited_amount float, p_credited_amount float);
+CREATE OR REPLACE FUNCTION "public".get_new_subledger_balance(p_subledger_oid varchar(128), p_debited_amount float default 0, p_credited_amount float default 0)
+RETURNS float AS $get_new_subledger_balance$
+    DECLARE
+        v_subledger_balance                      float;
+        v_debit_type                             varchar(16) := 'Debit';
+        v_credit_type                            varchar(16) := 'Credit';
+    BEGIN
+        select coalesce ((
+            select (
+                case when subledger_type = v_debit_type and p_debited_amount > p_credited_amount then (subledger_balance + p_debited_amount)
+                when subledger_type = v_debit_type and p_debited_amount < p_credited_amount then (subledger_balance - p_credited_amount)
+                when subledger_type = v_credit_type and p_debited_amount > p_credited_amount then (subledger_balance - p_debited_amount)
+                when subledger_type = v_credit_type and p_debited_amount < p_credited_amount then (subledger_balance + p_credited_amount)
+                else 0 end
+            )
+            from subledger 
+            where oid = p_subledger_oid
+        ), 0) into v_subledger_balance;
+    END;
+$get_new_subledger_balance$ language plpgsql;
+
+DROP FUNCTION IF EXISTS "public".post_journal(p_data json);
+CREATE OR REPLACE FUNCTION "public".post_journal(p_data json)
+RETURNS void AS $post_journal$
+    DECLARE
+        v_amount                        record;
+        v_journal                       json;
+        v_ledger_balance                float;
+        v_subledger_balance             float;
+        v_debited_amount                float;
+        v_credited_amount               float;
+        v_counter                       int := 0;
+        v_js_oid                        varchar(128);
+        v_company                       json;
+        v_fp                            json;
+        v_debit_type                    varchar(16) := 'Debit';
+        v_credit_type                   varchar(16) := 'Credit';
+    BEGIN
+        select get_company_by_login_id(p_data->>'created_by') into v_company;
+        select get_financial_period_by_company_oid(v_company->>'oid') into v_fp;
+
+        select sum((t->>'debited_amount')::float) as debited_amount, sum((t->>'credited_amount')::float) as credited_amount 
+        into v_amount 
+        from json_array_elements((p_data->>'journal_list')::json) t;
+
+        if v_amount.debited_amount != v_amount.credited_amount then 
+            raise exception 'debit amount is not equal to credit amount for %', v_company->>'short_name';
+            return;
+        end if;
+
+        select uuid() into v_js_oid;
+
+        insert into journal_summary (oid, journal_date, journal_type, journal_manner, description, amount, reference_no,   financial_period_oid, company_oid, created_by)
+        values (v_js_oid, current_data, nullif(p_data->>'journal_type', ''), coalesce(p_data->>'journal_manner', 'Auto'),
+        nullif(p_data->>'description', ''), v_amount.credited_amount, 
+        nullif(p_data->>'reference_no', ''), v_fp->>'oid', v_company->>'oid', p_data->>'created_by');
+
+        FOR v_journal in select * from json_array_elements((p_data->>'journal_list')::json) Loop 
+                v_counter := v_counter + 1;
+                v_debited_amount := coalesce((v_journal->>'debited_amount')::float, 0);
+                v_credited_amount := coalesce((v_journal->>'credited_amount')::float, 0);
+
+                select get_new_ledger_balance(v_journal->>'ledger_oid', v_debited_amount, v_credited_amount) into v_ledger_balance;
+                select get_new_subledger_balance(v_journal->>'subledger_oid', v_debited_amount, v_credited_amount) into v_subledger_balance;
+
+            insert into journal (oid, entry_no, description, debited_amount, credited_amount, ledger_oid, ledger_balance, subledger_oid, subledger_balance, journal_summary_oid,  financial_period_oid, company_oid, created_by)
+            values (uuid(), v_counter, nullif(v_journal->>'description', ''), v_debited_amount, v_credited_amount, v_journal->>'ledger_oid', v_ledger_balance, v_journal->>'subledger_oid', v_subledger_balance,
+            v_js_oid, v_fp->>'oid', v_company->>'oid', p_data->>'created_by');
+
+            update ledger set ledger_balance = v_ledger_balance 
+            where oid = v_journal->>'ledger_oid';
+
+            update subledger set subledger_balance = v_subledger_balance
+            where oid = v_journal->>'subledger_oid';
+        END LOOP;
+    END;
+$post_journal$ language plpgsql;
+
+--PGPASSWORD='password' psql -h localhost -U postgres -d gds -f ./R__05_03_04__journal.sql
---
+++ database/postgres/05-function/03-accounting/R__05_03_05__financial-period.sql
@@ -0,0 +1,69 @@
+set client_min_messages = 'warning';
+
+DROP FUNCTION IF EXISTS "public".get_financial_period_by_company_oid(p_company_oid varchar(128));
+CREATE OR REPLACE FUNCTION "public".get_financial_period_by_company_oid(p_company_oid varchar(128))
+RETURNS json AS $get_financial_period_by_company_oid$
+    DECLARE 
+        fp                          record;
+    BEGIN
+        select oid, start_date, end_date, lower(status) as status into fp
+        from financial_period 
+        where company_oid = p_company_oid
+        order by end_date desc limit 1;
+
+        if fp.oid is null then 
+            raise exception 'Not found financial_period - %', p_company_oid;
+        end if;
+
+        if fp.status is null or fp.status != 'opened' then
+            raise exception 'Not found financial_period as opened - %', fp.oid;
+        end if;
+
+        return row_to_json(fp);
+    END;
+$get_financial_period_by_company_oid$ language plpgsql;
+
+DROP FUNCTION IF EXISTS "public".close_financial_period(p_json json);
+CREATE OR REPLACE FUNCTION "public".close_financial_period(p_json json)
+RETURNS void AS $close_financial_period$
+    DECLARE
+        v_company                   json;
+        v_fp                        json;
+        v_revenue                   float;
+    BEGIN
+        select get_company_by_login_id(p_json->>'created_by') into v_company;
+        select get_financial_period_by_company_oid(v_company->>'oid') into v_fp;
+
+        select (sum(case when lg.ledger_group_code = '04' then l.ledger_balance else 0 end) - sum(case when lg.ledger_group_code = '05' then l.ledger_balance else 0 end))
+        into v_revenue
+        from ledger_group lg, ledger_subgroup lsg, ledger l 
+        where lg.oid = lsg.ledger_group_oid
+        and lsg.oid = l.ledger_subgroup_oid
+        and lg.company_oid = v_company->>'oid';
+
+        update ledger set ledger_balance = v_revenue
+        where oid = (select ledger_oid from ledger_setting where ledger_key = 'RetainedEarning');
+
+        insert into ledger_history (oid, ledger_code, ledger_name, mnemonic, ledger_type, balance_sheet_item, initial_balance,
+        ledger_balance, status, ledger_subgroup_oid, financial_period_oid, company_oid, created_by, parent_oid)
+        select uuid(), ledger_code, ledger_name, mnemonic, ledger_type, balance_sheet_item, initial_balance, ledger_balance, status, ledger_subgroup_oid, p_json->>'oid', company_oid, v_company->>'login_id', oid 
+        from ledger
+        where company_oid = v_company->>'oid';
+
+        update subledger_history
+        set ledger_history_oid = (select oid from ledger_history where parent_oid = ledger_oid limit 1)
+        where company_oid = v_company->>'oid' and financial_period_oid = p_json->>'oid';
+
+        update financial_period set status = 'Closed' where oid = p_json->>'oid' and company_oid = v_company->>'oid';
+
+        update ledger set initial_balance = ledger_balance, ledger_balance = 0.00
+        where company_oid = v_company->>'oid'
+        and (ledger_code like '04%' or ledger_code like '05%');
+
+        update subledger set initial_balance = subledger_balance, subledger_balance = 0.00
+        where company_oid = v_company->>'oid'
+        and (subledger_code like '04%' or subledger_code like '05%');
+    END;
+$close_financial_period$ language plpgsql;
+
+-- PGPASSWORD='password' psql -h localhost -U postgres -d gds -f ./R__05_03_05__financial-period.sql
